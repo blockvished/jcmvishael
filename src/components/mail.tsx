2cca41472d9bdb5a46b22e0d9ca16a3fd91a5e79
blockvished <jcmvishael@gmail.com>
2025-08-28 13:46:51 +0000

    use component

--- src/components/mail.tsx
+++ src/components/mail.tsx
@@ -1,13 +1,12 @@
 import { Mail } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default function EMail() {
   return (
-    <a
-      href="mailto:jcmvishael@gmail.com"
-      className="flex items-center gap-2 border border-gray-300 dark:border-gray-700 px-4 py-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition"
-    >
-      <Mail className="h-5 w-5" />
-      {/*<span className="text-sm">Email</span>*/}
+    <a href="mailto:jcmvishael@gmail.com">
+      <Button variant="outline" className="flex items-center gap-2">
+        <Mail className="h-5 w-5" />
+      </Button>
     </a>
   );
 }
